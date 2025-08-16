55e77a4a1a88659a46e2daf689d7a902a342398a
Abdallah Sauafth <asauafth@c2r3s7.42amman.com>
2025-08-16 14:43:30 +0300

    libft

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,13 +6,11 @@
 /*   By: asauafth <asauafth@Amman.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 15:58:42 by asauafth          #+#    #+#             */
-/*   Updated: 2025/08/14 19:09:00 by asauafth         ###   ########.fr       */
+/*   Updated: 2025/08/14 19:10:25 by asauafth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// #include <stdio.h>
 
 static int	word_count(const char *s, char c)
 {
